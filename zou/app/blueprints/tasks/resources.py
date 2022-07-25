c7c4b659a385c03b737ce2aacf190048cd7da134
NehmatH <nehmat_h@hotmail.com>
2022-07-25 14:56:27 +0200

    Modify documentation of put method

--- zou/app/blueprints/tasks/resources.py
+++ zou/app/blueprints/tasks/resources.py
@@ -47,7 +47,8 @@ class AddPreviewResource(Resource):
             name: task_id
             required: True
             schema:
-                type: UUID
+                type: integer
+                format: UUID
                 example: a24a6ea4-ce75-4665-a070-57453082c25
           - in: path
             name: comment_id
